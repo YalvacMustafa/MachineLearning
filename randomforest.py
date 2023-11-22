c2a598468bbce5167497c4eb52bd5320fa16251a
YalvacMustafa <zapza574@gmail.com>
2023-11-22 21:25:42 +0300

    güncelleme

--- randomforest.py
+++ randomforest.py
@@ -9,6 +9,6 @@ level = data.iloc[:, 1].values.reshape(-1, 1)
 salary = data.iloc[:, 2].values
 
 regression = RandomForestRegressor(n_estimators = 10, random_state=0) #estimators = kaç tane decision tree oluşturcak ona karar verir.
-regression.fit(level, salary)
+regression.fit(level, salary) #random_state sürekli aynı algoritma çalıştırır. Değer değişmez.
 
 print(regression.predict([[8.3]]))
